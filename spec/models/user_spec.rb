8df4e7d13f8e7ee7f631f28fae32b2c78d24887b
Vladyslav Beliaiev <vladyslav.beliaiev@builder.ai>
2025-07-03 11:13:44 +0200

    Fix test in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,14 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should have_many(:organisations_users).dependent(:destroy) }
   it { should have_many(:organisations).through(:organisations_users) }
-  it { should belong_to(:current_organisation).class_name('Organisation').optional }
+
+  context 'with pre-built user' do
+    subject { create(:user) }
+    it { should belong_to(:current_organisation).class_name('Organisation').optional }
+  end
 
   describe 'after_create :ensure_personal_organisation' do
-    let(:user) { create(:user, current_organisation: nil) }
+    let(:user) { create(:user) }
 
     it 'creates a personal organisation and sets it as current_organisation' do
       expect(user.current_organisation).to be_present
